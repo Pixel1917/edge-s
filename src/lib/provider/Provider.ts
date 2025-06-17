fce0c08204573709d249d76d26e61a1ba40440be
Dante Must Die <sergey.galaktionov1917@yandex.ru>
2025-06-17 07:42:13 +0300

    fix: 🐛 type fixes

--- src/lib/provider/Provider.ts
+++ src/lib/provider/Provider.ts
@@ -1,4 +1,8 @@
-import { createState, createDerivedState, createRawState } from '../store/index.js';
+import {
+	createState as BaseCreateState,
+	createDerivedState as BaseCreateDerivedState,
+	createRawState as BaseCreateRawState
+} from '../store/index.js';
 import { RequestContext } from '../context/index.js';
 import { browser } from '$app/environment';
 import type { Writable } from 'svelte/store';
@@ -6,19 +10,24 @@ import type { Writable } from 'svelte/store';
 type StoreDeps = {
 	createRawState: <T>(initial: T | (() => T)) => { value: T };
 	createState: <T>(initial: T | (() => T)) => Writable<T>;
-	createDerivedState: typeof createDerivedState;
+	createDerivedState: typeof BaseCreateDerivedState;
+};
+
+type NoConflict<I> = {
+	[K in keyof I]: K extends keyof StoreDeps ? never : I[K];
 };
 
 const globalClientCache = new Map<string, unknown>();
 
 export const createProvider = <T, I extends Record<string, unknown> = Record<string, unknown>>(
 	name: string,
-	factory: (args: StoreDeps & I) => T,
+	factory: (args: StoreDeps & NoConflict<I>) => T,
 	inject?: I
 ): (() => T) => {
 	const cacheKey = name;
+	let stateCounter = 0;
 
-	return () => {
+	return (): T => {
 		let contextMap: Map<string, unknown>;
 		if (browser) {
 			contextMap = globalClientCache;
@@ -34,22 +43,20 @@ export const createProvider = <T, I extends Record<string, unknown> = Record<str
 			return contextMap.get(cacheKey) as T;
 		}
 
-		let stateCounter = 0;
-
 		const autoKeyDeps = {
 			...inject,
-			createState: <T>(initial: T | (() => T)) => {
+			createState: <R>(initial: R | (() => R)) => {
 				const key = `${cacheKey ?? 'provider'}::state::${stateCounter++}`;
-				const initFn = typeof initial === 'function' ? (initial as () => T) : () => initial;
-				return createState(key, initFn);
+				const initFn = typeof initial === 'function' ? (initial as () => R) : () => initial;
+				return BaseCreateState<R>(key, initFn);
 			},
-			createRawState: <T>(initial: T | (() => T)) => {
+			createRawState: <RS>(initial: RS | (() => RS)) => {
 				const key = `${cacheKey ?? 'provider'}::rawstate::${stateCounter++}`;
-				const initFn = typeof initial === 'function' ? (initial as () => T) : () => initial;
-				return createRawState(key, initFn);
+				const initFn = typeof initial === 'function' ? (initial as () => RS) : () => initial;
+				return BaseCreateRawState<RS>(key, initFn);
 			},
-			createDerivedState
-		} as StoreDeps & I;
+			createDerivedState: BaseCreateDerivedState
+		} as StoreDeps & NoConflict<I>;
 
 		const instance = factory(autoKeyDeps);
 		if (cacheKey) {
@@ -60,7 +67,7 @@ export const createProvider = <T, I extends Record<string, unknown> = Record<str
 };
 
 export const createProviderFactory = <I extends Record<string, unknown>>(inject: I) => {
-	return function createInjectedProvider<T>(name: string, factory: (args: StoreDeps & I) => T): () => T {
+	return function createInjectedProvider<T>(name: string, factory: (args: StoreDeps & NoConflict<I>) => T): () => T {
 		return createProvider(name, factory, inject);
 	};
 };
